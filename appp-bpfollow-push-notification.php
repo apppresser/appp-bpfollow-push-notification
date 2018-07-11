d4fd15f9129ff674374e300be3890363fc3fb720
Matt Thiessen <matt@thiessen.us>
2018-07-11 16:32:35 -0500

    push notification when new activity gets posted

--- appp-bpfollow-push-notification.php
+++ appp-bpfollow-push-notification.php
@@ -17,6 +17,43 @@ class AppPush_BPFollow {
 
 	public function hooks() {
 		add_action('bp_follow_start_following', array( $this, 'appp_bpfollow_push_notification' ) );
+		add_action('bp_activity_after_save', array( $this, 'appp_new_activtity_send_push' ) );
+	}
+
+	/**
+	 * 
+	 * @param $activity BP_Activity_Activity
+	 */
+	public function appp_new_activtity_send_push( $activity ) {
+		$args = array(
+			'user_id' => $activity->user_id // the user posting the activity
+		);
+
+		$leader = new WP_User( $activity->user_id );
+		
+		// Does this person have any followers?
+		$follower_IDs = bp_follow_get_followers( $args );
+
+		if( $follower_IDs ) {
+
+			$appp_push = new AppPresser_Notifications_Update();
+
+			// push notification
+			$devices = $appp_push->get_devices_by_user_id( $follower_IDs );
+
+			if( !empty( $devices ) ) {
+				$send_date = 'now';
+				$content   = sprintf( __( '%s just posted new content', 'apppush-bpfollow' ), $leader->user_login );
+				$badges = 0;
+				$data = array();
+				$custom_url = '';
+				$custom_page = '';
+				$segment = '';
+				$target = '';
+			
+				$response = $appp_push->notification_send( $send_date, $content, $badges, $devices, $data, $custom_url, $custom_page, $segment, $target );
+			}
+		}
 	}
 
 	public function appp_bpfollow_push_notification( $follow ) {
